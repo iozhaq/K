b1eab290790c383f0789d020a647d310b404ab10
fankay <fankai@kaishengit.com>
2017-11-27 10:14:59 +0800

    演示Hibernate

---
+++ hibernate/src/main/java/com/kaishengit/pojo/Product.java
@@ -0,0 +1,41 @@
+package com.kaishengit.pojo;
+
+public class Product {
+
+    private Integer id;
+    private String productName;
+    private Integer productInventory;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getProductInventory() {
+        return productInventory;
+    }
+
+    public void setProductInventory(Integer productInventory) {
+        this.productInventory = productInventory;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", productName='" + productName + '\'' +
+                ", productInventory=" + productInventory +
+                '}';
+    }
+}
